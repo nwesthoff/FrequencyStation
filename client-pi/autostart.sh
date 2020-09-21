1759e543f765ae6850d2dccf958e364944561707
Nils Westhoff <nwesthoff@conceptivate.nl>
2020-09-21 12:47:39 +0200

    🐛 Fix startup script

--- client-pi/autostart.sh
+++ client-pi/autostart.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/IO3900-BSc-Final-Project-Demos/FrequencyStation/client-pi/ #where the script is
+cd home/pi/FrequencyStation/client-pi/ #where the script is
 python3 start.py #a command to run the script
 cd /
