cfdc6bca799a68cc0882f0c355c999b9c2401108
nwesthoff <nwesthoff@conceptivate.nl>
2020-09-23 15:19:34 +0100

    Use i2clibraries

--- client-pi/autostart.sh
+++ client-pi/autostart.sh
@@ -3,6 +3,8 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/FrequencyStation/client-pi/ #where the script is
+cd ~/FrequencyStation/client-pi/ #where the script is
+export QUICK2WIRE_API_HOME=~/FrequencyStation/client-pi/q2w
+export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
 python3 start.py #a command to run the script
 cd /
