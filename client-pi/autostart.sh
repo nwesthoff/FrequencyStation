927ea5ee54708b17f9c453df2fb702ccc29953d0
nwesthoff <nwesthoff@conceptivate.nl>
2020-09-24 13:46:22 +0100

    Fix the autostart to actually source right

--- client-pi/autostart.sh
+++ client-pi/autostart.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
 cd ~/FrequencyStation/client-pi/ #where the script is
-export QUICK2WIRE_API_HOME=~/FrequencyStation/client-pi/q2w
-export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
+source setup.env
 python3 start.py #a command to run the script
 cd /
