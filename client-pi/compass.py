726bc9ca70be6b0cbc73ed6f57911df3fc4e1bea
Nils Westhoff <nwesthoff@conceptivate.nl>
2020-09-24 09:53:38 +0200

    ✨ Create separate compass test application

---
+++ client-pi/compass.py
@@ -0,0 +1,22 @@
+from envirophat import leds
+import time
+from i2clibraries import i2c_hmc5883l
+
+# INITIALISATION START
+
+compass = i2c_hmc5883l.i2c_hmc5883l(1)
+compass.setContinuousMode()
+compass.setDeclination(1, 47)
+ß
+# INITIALISATION DONE
+
+
+try:
+    print('started compass')
+    while True:
+        heading = compass.getHeading()
+        print(heading)
+        time.sleep(.2)
+
+except KeyboardInterrupt:
+    leds.off()
